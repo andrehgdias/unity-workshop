5af1c4f69c0c1bca6a29a04d32ef225167983d57
André Dias <andredias.us@gmail.com>
2021-11-06 17:47:17 -0300

    feat: Headbob adicionado ao andar e correr

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -10,8 +10,16 @@ public class FirstPersonController : MonoBehaviour
     [Header("Movement Parameters")]
     [SerializeField] private float walkSpeed = 4f;
     [SerializeField] private float sprintSpeed = 8f;
-    [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private float gravity = 13f;
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float gravity = 15f;
+
+    [Header("Headbob Parameters")]
+    [SerializeField] private float walkBobSpeed = 10f;
+    [SerializeField] private float walkBobAmountH = 0.025f;
+    [SerializeField] private float walkBobAmountV = 0.05f;
+    [SerializeField] private float sprintBobSpeed = 13.5f;
+    [SerializeField] private float sprintBobAmountH = 0.08f;
+    [SerializeField] private float sprintBobAmountV = 0.15f;
 
     [Header("Camera Parameters")]
     [SerializeField, Range(1, 10)] private float mouseSensitivityX = 2f;
@@ -24,11 +32,16 @@ public class FirstPersonController : MonoBehaviour
     private Vector3 moveDirection;
     private Vector2 currentInput;
     private float rotationAroundX = 0;
+    private float defaultXPosition;
+    private float defaultYPosition;
+    private float timer;
 
     void Awake()
     {
         playerCamera = GetComponentInChildren<Camera>();
         characterController = GetComponent<CharacterController>();
+        defaultXPosition = playerCamera.transform.localPosition.x;
+        defaultYPosition = playerCamera.transform.localPosition.y;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -45,7 +58,7 @@ public class FirstPersonController : MonoBehaviour
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
         float moveDirectionY = moveDirection.y;
         moveDirection = transform.forward * currentInput.x + transform.right * currentInput.y;
         moveDirection.y = moveDirectionY;
@@ -59,17 +72,31 @@ public class FirstPersonController : MonoBehaviour
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
     }
 
+    private void HandleHeadBob()
+    {
+        if (!characterController.isGrounded) return;
+
+        if(Mathf.Abs(moveDirection.x) > 0.1f || Mathf.Abs(moveDirection.z) > 0.1f)
+        {
+            timer += Time.deltaTime * (IsSprinting ? sprintBobSpeed : walkBobSpeed);
+            playerCamera.transform.localPosition = new Vector3(
+                defaultXPosition + Mathf.Cos(timer) * (IsSprinting ? sprintBobAmountH : walkBobAmountH),
+                defaultYPosition + Mathf.Sin(timer) * (IsSprinting ? sprintBobAmountV : walkBobAmountV),
+                playerCamera.transform.localPosition.z
+            );
+        }
+    }
+
     private void ApplyFinalMovements()
     {
         if (!characterController.isGrounded)
             moveDirection.y -= gravity * Time.deltaTime;
         else if(Input.GetButtonDown("Jump"))
-        {
             moveDirection.y = jumpForce;
-        }
         else
-            moveDirection.y = -2f;
+            moveDirection.y = -1f;
             
-        characterController.Move(moveDirection * (IsSprinting ? sprintSpeed : walkSpeed) * Time.deltaTime);
+        characterController.Move(moveDirection * Time.deltaTime);
+        HandleHeadBob();
     }
 }
