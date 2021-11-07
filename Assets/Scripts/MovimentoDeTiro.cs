8e21b2dae54e4eea5092b1d13e675e067ab886fb
maykborgs <maaykon51@gmail.com>
2021-11-07 15:07:48 -0300

    feat: teleportation weapon

--- Assets/Scripts/MovimentoDeTiro.cs
+++ Assets/Scripts/MovimentoDeTiro.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class MovimentoDeTiro : MonoBehaviour
 {
     public float lifeTime = 3;
-
+    public GameObject player;
     public float velocity = 0.1f;
 
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     // Update is called once per frame
     void Update()
     {
@@ -25,6 +31,7 @@ public class MovimentoDeTiro : MonoBehaviour
     {
         Debug.Log(gameObject.name);
         Debug.Log(collision.gameObject.tag);
+        Debug.Log(player.name);
         if (gameObject.name == "RocketEmpty(Clone)" && collision.gameObject.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
@@ -32,7 +39,9 @@ public class MovimentoDeTiro : MonoBehaviour
         }    
         if (gameObject.name == "TeletransporteBullet(Clone)" && collision.gameObject.tag == "Parede")
         {
-            
+            Debug.Log("acertei uma parede");
+            player.transform.position = gameObject.transform.GetChild(0).gameObject.transform.position;
+            Destroy(gameObject);
             // o player faz o tp pra esse local
         }
         
